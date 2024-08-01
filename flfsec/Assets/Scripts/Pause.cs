c33299866c7fe17c8b78ac9b1ffcab725a4b5176
Kyungho Jeon <jkhcoding@naver.com>
2024-08-01 16:52:06 +0900

    pause setting fix

--- flfsec/Assets/Scripts/Pause.cs
+++ flfsec/Assets/Scripts/Pause.cs
@@ -9,9 +9,13 @@ public class Pause : MonoBehaviour
     private bool IsPause;
     private bool IsResume;
     private bool IsDelayActive;
+    public bool IsSettingOpened;        // 설정 메뉴 표시 확인 변수
 
     public Image pauseSprite;
     public Animator uiAnimator;
+    public GameObject settingsMenu;     // 설정 메뉴 UI
+    public Button settingsButton;       // 설정 메뉴 열기 버튼
+    public Button backButton;           // 설정 메뉴 닫기 버튼
 
     private readonly string showTrigger = "doShow";
     private readonly string hideTrigger = "doHide";
@@ -19,7 +23,8 @@ public class Pause : MonoBehaviour
     void Start()
     {
         settingTab = FindObjectOfType<SettingTab>();
-
+        IsSettingOpened = false;
+        settingsMenu.SetActive(false);
         IsPause = false;
         IsResume = false;
         IsDelayActive = false;
@@ -31,26 +36,34 @@ public class Pause : MonoBehaviour
             uiAnimator.ResetTrigger(showTrigger);
             uiAnimator.ResetTrigger(hideTrigger);
         }
+        if (settingsButton != null)
+            settingsButton.onClick.AddListener(OpenSettings);
+
+        if (backButton != null)
+            backButton.onClick.AddListener(CloseSettings);
     }
 
     void Update()
     {
-        if (IsDelayActive) return;
+        // 지연 상태라면 아무것도 하지 않음
+        if (IsDelayActive)
+            return;
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (settingTab.IsSettingOpened)
-            {
-                settingTab.CloseSettings();
-            }
-            else if (!IsPause)
+            // 실행 중 esc키가 눌린 경우 -> 게임 정지
+            if (!IsPause)
             {
                 PauseGame();
             }
-            else if (IsPause && !IsResume)
-            {
+            // 정지 상태이고 설정 창이 켜져 있는 경우 -> 설정창 꺼짐
+            else if (IsPause && IsSettingOpened)
+                CloseSettings(); // 설정창이 열려있으면 닫기
+
+            // 정지 상태이고 설정 창이 꺼져있는 경우 -> 게임 재개
+            else if (IsPause && !IsSettingOpened)
                 StartCoroutine(ResumeGameAfterDelay(3.0f));
-            }
+
         }
     }
 
@@ -74,6 +87,18 @@ public class Pause : MonoBehaviour
         {
             StartCoroutine(ResumeGameAfterDelay(3.0f));
         }
+    }
+
+    public void OpenSettings()
+    {
+        IsSettingOpened = true;
+        settingsMenu.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        IsSettingOpened = false;
+        settingsMenu.SetActive(false);
     }
 
     IEnumerator ResumeGameAfterDelay(float delay)
